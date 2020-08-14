e4bdd8ceea77f96ce4dd9e3cd2dabadaad6dc96f
lgh7418 <lgh7418@gmial.com>
2020-08-14 14:30:02 +0900

    refactor schema, confirmEmail

---
+++ src/routes/confirmEmail.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { redis } from "../redis";
+import { User } from "../entity/User";
+
+export const confirmEmail = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const userId = await redis.get(id);
+  if (userId) {
+    await User.update({ id: userId }, { confirmed: true });
+    res.send("ok");
+  } else {
+    res.send("invalid");
+  }
+};
--- src/startServer.ts
+++ src/startServer.ts
@@ -1,39 +1,19 @@
 import { GraphQLServer } from "graphql-yoga";
-import { loadFilesSync } from "@graphql-tools/load-files";
-import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { createTypeormConn } from "./utils/createTypeormConn";
-import * as Redis from "ioredis";
-import { User } from "./entity/User";
+import { redis } from "./redis";
+import { confirmEmail } from "./routes/confirmEmail";
+import { genSchema } from "./utils/genSchema";
 
 export const startServer = async () => {
-  const typeDefs = mergeTypeDefs(
-    loadFilesSync(`${__dirname}/modules/**/*.graphql`)
-  );
-  const resolvers = mergeResolvers(
-    loadFilesSync(`${__dirname}/modules/**/resolvers.ts`)
-  );
-
-  const redis = new Redis();
-
   const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
+    schema: genSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + "://" + request.get("host"),
     }),
   });
 
-  server.express.get("/confirm/:id", async (req, res) => {
-    const { id } = req.params;
-    const userId = await redis.get(id);
-    if (userId) {
-      await User.update({ id: userId }, { confirmed: true });
-      res.send("ok");
-    } else {
-      res.send("invalid");
-    }
-  });
+  server.express.get("/confirm/:id", confirmEmail);
 
   await createTypeormConn();
   const port = process.env.NODE_ENV === "test" ? 0 : 4000;
